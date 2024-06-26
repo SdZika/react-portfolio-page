6a0cccaca098bb563cff494131e75e58c56ee6f1
SdZika <markozivkovic1982@gmail.com>
2024-06-26 13:04:28 +0200

    first commit

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,6 @@
+export const Footer = () => (
+    <footer>
+        <div className="icons"></div>
+        <p>Copyright @2024 All rights reserved</p>
+    </footer>
+)
---
+++ src/Components/Nav/index.tsx
@@ -0,0 +1,12 @@
+import './Nav.styles.css'
+
+export const Nav = ()=> (
+    <nav>
+        <ul>
+            <li><a href="">Works</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Contacts</a></li>
+        </ul>
+    </nav>
+   
+)
